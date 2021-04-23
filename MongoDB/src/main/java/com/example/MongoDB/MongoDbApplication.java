9cc9ae9926a03c91088444b53f5bad3a7200f43f
DESKTOP-IVO4IU0\mahfo <mahfooz.lakhani@gmail.com>
2021-04-24 05:22:19 +0530

    Added code for CRUD operation

--- MongoDB/src/main/java/com/example/MongoDB/MongoDbApplication.java
+++ MongoDB/src/main/java/com/example/MongoDB/MongoDbApplication.java
@@ -2,10 +2,12 @@ package com.example.MongoDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.example.repository")
+@ComponentScan("com.example.*")
 public class MongoDbApplication {
 
   public static void main(String[] args) {
