a7e62928cc41a79e9d7121510de8b0e7b9727d0c
DESKTOP-IVO4IU0\mahfo <mahfooz.lakhani@gmail.com>
2021-04-24 02:10:13 +0530

    Added Repository for Student Document and modified MongoDbApplication.java to enable MongoRepositories

--- MongoDB/src/main/java/com/example/MongoDB/MongoDbApplication.java
+++ MongoDB/src/main/java/com/example/MongoDB/MongoDbApplication.java
@@ -2,12 +2,14 @@ package com.example.MongoDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class MongoDbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongoDbApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MongoDbApplication.class, args);
+  }
 
 }
